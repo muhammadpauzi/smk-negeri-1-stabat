e6650b9712da4850352c5fb22e2bd1d62b8046d2
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-04-16 08:41:19 +0700

    create MajorPolicy (shoudl before previous commit)

---
+++ app/Policies/MajorPolicy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Major;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class MajorPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function update(User $user, Major $major)
+    {
+        return $user->isSuperadmin();
+    }
+
+    public function delete(User $user, Major $major)
+    {
+        return $user->isSuperadmin();
+    }
+}
